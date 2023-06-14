273400900966e36cf30ec0b6bc45a5d11a8332f9
Bipashakh1267 <131987861+Bipashakh1267@users.noreply.github.com>
2023-06-14 19:56:54 +0530

    Update setupUsers.sh

--- setupUsers.sh
+++ setupUsers.sh
@@ -1,26 +1,33 @@
 #!/bin/bash
 if [ $# -eq 0 ]; then
    while true; do
-    read -p "Enter name: " name
-    echo "$name" >> temp.txt
+      read -p "Enter name: " name
+      if id "$name" >/dev/null 2>&1; then
+         echo "User $name already registered"
+      else
+         sudo useradd -m -d /home/$name $name
+         sudo touch /home/$name/website.txt
+         sudo chown $name:$name /home/$name/website.txt
+         sudo chmod 446 /home/$name/website.txt
+      fi
    done
-    file=temp.txt
 else
-    file=$1
-    if [ ! -f "$file" ]; then
+   file=$1
+   if [ ! -f "$file" ]; then
       echo "Error: $file not found"
       exit 1
-    fi
-fi
-cat $file | while read line; do
-   name=$(echo $line | awk '{print $1}')
-   
-   if id "$name" >/dev/null 2>&1; then
-      echo "User $name already registered"
    else
-      sudo useradd -m -d /home/$name $name
-      sudo touch /home/$name/website.txt
-      sudo chown $name:$name /home/$name/website.txt
-      sudo chmod 446 /home/$name/website.txt
+      while read -r line; do
+         name=$(echo "$line" | awk '{print $1}')
+   
+         if id "$name" >/dev/null 2>&1; then
+            echo "User $name already registered"
+         else
+            sudo useradd -m -d /home/$name $name
+            sudo touch /home/$name/website.txt
+            sudo chown $name:$name /home/$name/website.txt
+            sudo chmod 446 /home/$name/website.txt
+         fi
+      done < "$file"
    fi
-done
+fi
